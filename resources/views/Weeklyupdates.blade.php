ec78a3b26148825dc1a95ac11f04c4823b8c4f24
SIMBA <simban6@gmail>
2020-01-21 22:32:13 +0000

    new stuff 1

--- resources/views/Weeklyupdates.blade.php
+++ resources/views/Weeklyupdates.blade.php
@@ -14,7 +14,7 @@
                 <h1><Strong>Pride</Strong></h1>
                 <p>This weeks topic was suggested to me by a very good friend of mine that i value highly. One thing that we men have a big issue on is <strong>Pride</strong>. This is a common theme amongst men in being overly prideful in not sharing how they feel when faced with issues. It usually stems from the idea that we need to be 'men' and hold our feelings in and therefore sharing if we are depressed is seen as a vulnerability. In essence we would rather be prideful and suffer than actually share our true issues.<p>
                 <p> In my experience i myself have found myself on countless occasions not wanting to share how im feeling and the struggles im facing purely because of my pride. Because i did not want to be seen as weak, im still learning to not be prideful and many of us need to do stop getting in our own way. When in actuality true strength lies when we finally accept to be <strong>vulnerable</strong> and put our prides away. We need to learn to appreciate the help that is around us and go those when we are in need. Lets stop suffering in silence.</p>
-                <p>So how about we challenge this. Challenge this narrative and put our pride to the side, put our ego our aside and for once let ourselves be vulnerable</p>
+                <p>So how about we challenge this. Lets challenge this narrative and put our pride to the side, put our ego our aside and for once let ourselves be vulnerable</p>
                 <p> If you want to share your thoughts and opinions, tell me what you think via twitter (@Brotherhood0019)</p>
            <br>
                 <p><strong>Some new features included this week are:</strong></p>
