bbac482146e786031b10a25e1d54b817b14a6f51
SIMBA <simban6@gmail>
2020-06-05 21:34:58 +0100

    Admin 2

--- resources/views/AdminPost.blade.php
+++ resources/views/AdminPost.blade.php
@@ -14,7 +14,7 @@
     @include('partials.Sidebar')
     <div id="main" class="container">
         <h1><strong>{{ $adminpost->title }}</strong></h1>
-        <h2>{{ $adminpost->content }}</h2>
+        <h5>{{ $adminpost->content }}</h5>
         <div class="info">
             <strong><a href="{{ route('user.show', $adminpost->user_id) }}"><p class="text-black">Posted by {{ $adminpost->user->name }} {{ $adminpost->created_at->diffForHumans()}} </p></a></strong>
         </div>
