6f34c1a243e5f021d7795f5fbc809dfa8025ab79
SIMBA <simban6@gmail>
2020-06-06 12:01:02 +0100

    Admin 3

--- database/migrations/2020_06_06_103723_create_admin_posts_table.php
+++ database/migrations/2020_06_06_103723_create_admin_posts_table.php
@@ -16,8 +16,9 @@ class CreateAdminPostsTable extends Migration
 
         Schema::create('admin_posts', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('title');
-            $table->string('content');
+            $table->longText('content');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->rememberToken();
